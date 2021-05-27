4b49d4c23662cdd8173ccb874808b3709d02ca08
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2021-05-27 09:29:20 -0300

    Validação não pode ter dois usuários com o mesmo email.

--- src/main/java/br/com/zup/mercadolivre/users/UserDTO.java
+++ src/main/java/br/com/zup/mercadolivre/users/UserDTO.java
@@ -1,5 +1,7 @@
 package br.com.zup.mercadolivre.users;
 
+import br.com.zup.mercadolivre.validators.UniqueValue;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -8,6 +10,7 @@ public class UserDTO {
 
     @NotBlank
     @Email
+    @UniqueValue(domainClass = User.class, fieldName = "login")
     private String login;
 
     @NotBlank
